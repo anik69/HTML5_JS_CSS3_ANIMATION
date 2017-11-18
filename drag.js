4661f784b88893c523c3cd6943827605e70c572b
Md. Kamrul Islam. Anik <kamrulislam.anik@gmail.com>
2017-11-18 16:04:55 +0600

    first commit

---
+++ drag.js
@@ -0,0 +1,8 @@
+function handleDragStart(e) {
+  this.style.opacity = '0.4';  // this / e.target is the source node.
+}
+
+var cols = document.querySelectorAll('#columns .column');
+[].forEach.call(cols, function(col) {
+  col.addEventListener('dragstart', handleDragStart, false);
+});
